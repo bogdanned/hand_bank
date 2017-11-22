0c52fd22ab9d268852dfb7cafc121a9e3d5ff70f
Bogdan Mihai <bogdanned32@gmail.com>
2017-11-22 10:32:26 +0100

    webpack setup

---
+++ src/server/setup.js
@@ -0,0 +1,13 @@
+import bodyParser from 'body-parser'
+
+
+export default (app) => {
+  app.use(bodyParser.json()) // for parsing application/json
+  app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+  app.get('/test', (req, res) => {
+    res.send('test back')
+  })
+
+  return app
+}
