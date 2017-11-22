e8c21e13bd9bfa140f79c8127d9dea70d4986074
Dragos <dragosgn@gmail.com>
2017-11-22 11:34:29 +0100

    added front end stuff

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -2,16 +2,40 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Root = styled.div`
-  padding: 3rem;
+  display: flex;
 `
 const Sidebar = styled.div`
   width: 200px;
+  padding: 5rem 1rem 1rem 1rem;
   height: 100 vh;
+`
+
+const Content = styled.div`
+  width: 100%;
+`
+
+const MenuItem = styled.div`
+  padding: 0.5rem;
+  background-color: ${props => props.theme.brandColor};
+  color: white;
+  cursor: pointer;
 `
 
 export default () => (
   <Root>
-    <h1>Welcome to Handworks!</h1>
-    <Sidebar></Sidebar>
+    <Sidebar>
+      <MenuItem>
+        <p>Cashflow Prediction</p>
+      </MenuItem>
+      <MenuItem>
+        <p>Cashflow Prediction</p>
+      </MenuItem>
+      <MenuItem>
+        <p>Cashflow Prediction</p>
+      </MenuItem>
+    </Sidebar>
+    <Content>
+      <h1></h1>
+    </Content>
   </Root>
 )
