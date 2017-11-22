e8bbc7a181b1dd480f88bb4e7c8ec8111385f86d
Bogdan Mihai <bogdanned32@gmail.com>
2017-11-22 10:56:16 +0100

    webpack

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -7,8 +7,6 @@ import CompressionWebpackPlugin from "compression-webpack-plugin"
 import AssetsPlugin from 'assets-webpack-plugin'
 
 const isDev = true,
-      host = '127.0.0.1',
-      port = process.env.PORT || 5000
 
 const vendor = [
   "react",
@@ -21,99 +19,87 @@ const vendor = [
 ]
 
 export default {
-  devtool: isDev ? 'inline-source-map' : "hidden-source-map",
-  bail: false,
-  node: {
+  devtool : isDev
+    ? 'inline-source-map'
+    : "hidden-source-map",
+  bail : false,
+  node : {
     fs: false,
     Buffer: false
   },
-  entry: isDev ? {
-    app: [
-      'babel-polyfill',
-      'react-hot-loader/patch',
-      `webpack-dev-server/client?http://${host}:${port}`,
-      'webpack/hot/only-dev-server',
-      path.join(__dirname, '/src/client/index.js')
-    ],
-    vendor
-  } :
-  {
-    app: [
-    'babel-polyfill',
-    path.join(__dirname, '/src/client/index.js')
-  ],
-  vendor
-},
-  output: {
+  entry : isDev
+    ? {
+      app: [
+        'babel-polyfill',
+        'react-hot-loader/patch',
+        'webpack/hot/only-dev-server',
+        path.join(__dirname, '/src/client/index.js')
+      ],
+      vendor
+    }
+    : {
+      app: [
+        'babel-polyfill',
+        path.join(__dirname, '/src/client/index.js')
+      ],
+      vendor
+    },
+  output : {
     path: path.join(__dirname, "public"),
-    filename: isDev ? 'bundle-[name].js' : 'bundle-[name]-[hash].js',
-    publicPath: `${host}:${port}`
+    filename: isDev ? 'bundle-[name].js': 'bundle-[name]-[hash].js'
   },
-  devServer: {
+  devServer : {
     hot: true,
     historyApiFallback: true,
     contentBase: path.resolve(__dirname, "public"),
     disableHostCheck: true,
-    host,
     port,
     overlay: true,
-    setup(app){
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    setup(app) {
       let srcDir = path.resolve(__dirname, "src", "server")
       reloadable(app, {
         requireFile: path.join(srcDir, "./setup"),
         watch: srcDir,
         watchOpts: {
-          ignoreInitial: true,
+          ignoreInitial: true
         }
       })
-    },
+    }
   },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "vendor",
-      minChunks: Infinity,
-    }),
+  plugins : [
+    new webpack.optimize.CommonsChunkPlugin({name: "vendor", minChunks: Infinity}),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(),
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: "index.html.ejs",
-      chunks: ["vendor","app"],
+      chunks: ["vendor", "app"]
     }),
-    new CompressionWebpackPlugin({
-      asset: "[path].gz[query]",
-			algorithm: "gzip",
-			test: /\.(js|html)$/,
-			threshold: 10240,
-			minRatio: 0.8
-    }),
+    new CompressionWebpackPlugin({asset: "[path].gz[query]", algorithm: "gzip", test: /\.(js|html)$/, threshold: 10240, minRatio: 0.8}),
     new AssetsPlugin(),
     new webpack.NamedModulesPlugin(),
     new webpack.NoEmitOnErrorsPlugin()
     // do not emit compiled assets that include errors
   ],
-  module: {
+  module : {
     loaders: [
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader'
-      },
-      {
+      }, {
         test: /\.css$/,
         loader: [
-          "style-loader",
-          'css-loader?' + qs.stringify({
-            modules: false,
-            importLoaders: 1,
-            localIdentName: '[path][name]-[local]',
-          }),
+          "style-loader", 'css-loader?' + qs.stringify({modules: false, importLoaders: 1, localIdentName: '[path][name]-[local]'})
         ].join("!")
       }
-    ],
+    ]
   },
-  resolve: {
+  resolve : {
     extensions: ['.js']
   },
-  stats: "verbose"
+  stats : "verbose"
 }
