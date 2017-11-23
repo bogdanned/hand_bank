3d805655bd41fa3704797f56483b7f159ec7cad0
Bogdan Mihai <bogdanned32@gmail.com>
2017-11-23 08:33:55 +0100

    package

--- src/client/data/offer/services.js
+++ src/client/data/offer/services.js
@@ -2,39 +2,231 @@ export default[
   {
     name : "Shower Installation",
     quality : "Luxury",
-    id : "shower_tray",
+    id : "shower",
     products : [
       {
         name: "Shower tap",
+        id: "shower_tap",
         price: "139,99",
         producer: "HomeLavade"
       }, {
         name: "Shower cabin",
+        id: "shower_cabin",
         price: "269,00",
         producer: "Bernstein"
       }, {
-        name: "Shower tray",
-        price: "469,00",
-        producer: "Bernstein"
+        name: "Rectagular shower tray",
+        id: "shower_tray",
+        price: "240,38",
+        producer: "Villeroy & Boch"
       }, {
         name: "Rain Shower",
+        id: "rain_shower",
         price: "53,50",
         producer: "Bernstein"
       }
     ]
   }, {
     name : "Bathtub",
-    quality : "Luxory",
+    quality : "Luxury",
     id : "bath",
     products : [
       {
-        name: "Luna Stone",
+        name: "Luna Stone bathtub",
+        id: "bathtube",
         price: "1594,00",
         producer: "Bernstein"
       }, {
         name: "Tap",
+        id: "tap",
         price: "39,90",
         producer: "Oskar"
+      }
+    ]
+  }, {
+    name : "Toilet",
+    quality : "Luxury",
+    id : "toilet",
+    products : [
+      {
+        name: "iCon toilet",
+        id: "toilet_icon",
+        price: "289,90",
+        producer: "Kermang"
+      }, {
+        name: "Sanitary pipe",
+        id: "sanitary_pipe",
+        price: "20,89",
+        producer: "Gebo"
+      }, {
+        name: "Toilet paper holder",
+        id: "paper_holder",
+        price: "55,00",
+        producer: "Cairo"
+      }
+    ]
+  }, {
+    name : "Ligting",
+    quality : "Luxury",
+    id : "lighting",
+    products : [
+      {
+        name: "Ceiling lights",
+        id: "ceiling_lights",
+        price: "890,50",
+        producer: "BOPP"
+      }, {
+        name: "Accent lights",
+        id: "accent_light",
+        price: "44,99",
+        producer: "Philips"
+      }, {
+        name: "Wall lights",
+        id: "wall_lights",
+        price: "780,10",
+        producer: "Steng"
+      }
+    ]
+  }, {
+    name : "Cabinets and shelves",
+    quality : "Normal",
+    id : "lighting",
+    products : [
+      {
+        name: "Cabinet",
+        id: "cabinet",
+        price: "180,00",
+        producer: "Duravit"
+      }, {
+        name: "Medicine Cabinet",
+        id: "medicine_cabinet",
+        price: "279,96",
+        producer: "Duravit"
+      }, {
+        name: "Shelf",
+        id: "shelf",
+        price: "119,00",
+        producer: "blomus"
+      }
+    ]
+  }, {
+    name : "Accessories",
+    quality : "Luxury",
+    id : "accessories",
+    products : [
+      {
+        name: "Mirror",
+        id: "mirror",
+        price: "773,98",
+        producer: "FURA SPIEGEL"
+      }, {
+        name: "Soap holder",
+        id: "soap_holder",
+        price: "204,41",
+        producer: "Boffi"
+      }, {
+        name: "Towel ring",
+        id: "towel_ring",
+        price: "146,68",
+        producer: "GROHE"
+      }, {
+        name: "Towel bar",
+        id: "towel_bar",
+        price: "111,99 ",
+        producer: "pomd'or"
+      }
+    ]
+  }, {
+    name : "Combined bath and shower",
+    quality : "Normal",
+    id : "accessories",
+    products : [
+      {
+        name: "Edwardian Bath Shower Mixer Tap",
+        id: "combined_tap",
+        price: "170,00",
+        producer: "HomeLavade"
+      }, {
+        name: "Cobined cabin",
+        id: "combined_cabin",
+        price: "144,00",
+        producer: "Bernstein"
+      }, {
+        name: "Luna Stone bathtub",
+        id: "bathtube",
+        price: "1594,00",
+        producer: "Bernstein"
+      }
+    ]
+  }, {
+    name : "Sink set",
+    quality : "Luxury",
+    id : "sink_set",
+    products : [
+      {
+        name: "Sink",
+        id: "sink",
+        price: "949,45",
+        producer: "Villeroy & Boch"
+      }, {
+        name: "Sink tap",
+        id: "sink_tap",
+        price: "732,94",
+        producer: "BLANCO"
+      }
+    ]
+  }, {
+    name : "Flooring",
+    quality : "Luxury",
+    id : "accessories",
+    products : [
+      {
+        name: "Tiles",
+        id: "tile",
+        price: "150,00",
+        producer: "Kermang"
+      }, {
+        name: "Tiles fix",
+        id: "glue",
+        price: "120,00",
+        producer: "Heineker"
+      }
+    ]
+  }, {
+    name : "Paint",
+    quality : "Normal",
+    id : "paint",
+    products : [
+      {
+        name: "Paint Green",
+        id: "paint_green",
+        price: "52,00",
+        producer: "Heineker"
+      }
+    ]
+  }, {
+    name : "Plumbing",
+    quality : "Normal",
+    id : "plumbing",
+    products : [
+      {
+        name: "Water pipe",
+        id: "water_pipe",
+        price: "49,00",
+        producer: "JM Eagle"
+      }
+    ]
+  }
+  {
+    name: "Heating",
+    quality: "Normal",
+    id: "heating",
+    products: [
+      {
+        name: "Water heating cylinder",
+        id: "water_heating",
+        price: "1.287,63",
+        producer: "Stiebel"
       }
     ]
   }
